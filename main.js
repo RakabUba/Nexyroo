cfebbbcdb8bafecbf49efe7b011e3d9745001eb3
Nexyroo <abubakar.071103@gmail.com>
2019-10-04 17:27:15 +0700

    ADD

---
+++ main.js
@@ -0,0 +1,9 @@
+$('#banner').text("Hello, jQuery!");
+
+function sapadia(){
+    var angka_pertama = parseInt$('#angka_pertama').val();
+    var angka_kedua = parseInt$('#angka_kedua').val();
+    
+    var hasil = angka_pertama + angka_kedua;
+    $('#hasil').text(`hasil ${angka_pertama} + ${angka_kedua} = ${hasil}`);
+}
